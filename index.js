19563600b9cd8239dfbf6f24b4410b00c58d9e99
Victor Lindholm <125816815+lithium-lamp@users.noreply.github.com>
2024-09-02 08:51:17 +0200

    modified:   index.html 	modified:   index.js 	modified:   resources/json/posts.json 	new file:   resources/json/videos.json 	modified:   style.css

--- index.js
+++ index.js
@@ -26,4 +26,43 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         })
         .catch(error => console.error("Error fetching JSON data:", error));
+
+
+    fetch('resources/json/videos.json')
+    .then(response => response.json())
+    .then(data => {
+        for (var i = 0; i < data.length; i++){
+            const videoDisplay = document.getElementById("videoDisplay");
+
+            const titleElement = document.createElement("h3");
+            titleElement.textContent = data[i].title;
+
+            const dateElement = document.createElement("p");
+            dateElement.textContent = data[i].date;
+
+            videoDisplay.appendChild(titleElement);
+            videoDisplay.appendChild(dateElement);
+
+            if (data[i].link != "") {
+                const playerElement = document.createElement("div");
+                playerElement.className = "player";
+
+                const linkElement = document.createElement("iframe");
+                linkElement.setAttribute("src", data[i].link);
+                linkElement.setAttribute("title", "YouTube video player");
+                linkElement.setAttribute("frameborder", "0");
+                linkElement.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share");
+                linkElement.setAttribute("referrerpolicy", "strict-origin-when-cross-origin");
+                linkElement.setAttribute("allowfullscreen", "true");
+                linkElement.style.width = "320px";
+                linkElement.style.height = "180px";
+
+                playerElement.appendChild(linkElement)
+                videoDisplay.appendChild(playerElement);
+            }
+
+            videoDisplay.appendChild(document.createElement("hr"));
+        }
+    })
+    .catch(error => console.error("Error fetching JSON data:", error));
 });
