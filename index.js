a0241d67455073c9339e800999988b10945bc410
Victor Lindholm <125816815+lithium-lamp@users.noreply.github.com>
2024-09-23 13:53:49 +0200

    WIP 	modified:   github.js 	modified:   index.html 	modified:   index.js 	new file:   projects.css 	new file:   resources/json/contactinfo.json 	modified:   style.css

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
             for (var i = 0; i < data.length; i++){
                 const dataDisplay = document.getElementById("dataDisplay");
 
+                if (i == 0) {
+                    const countElement = document.createElement("h3");
+                    countElement.textContent = "(" + data.length + ") Pdf items";
+    
+                    dataDisplay.appendChild(countElement);
+    
+                    dataDisplay.appendChild(document.createElement("hr"));
+                }
+
                 const titleElement = document.createElement("h3");
                 titleElement.textContent = data[i].title;
 
@@ -33,6 +42,15 @@ document.addEventListener("DOMContentLoaded", function () {
     .then(data => {
         for (var i = 0; i < data.length; i++){
             const videoDisplay = document.getElementById("videoDisplay");
+
+            if (i == 0) {
+                const countElement = document.createElement("h3");
+                countElement.textContent = "(" + data.length + ") Video items";
+
+                videoDisplay.appendChild(countElement);
+
+                videoDisplay.appendChild(document.createElement("hr"));
+            }
 
             const titleElement = document.createElement("h3");
             titleElement.textContent = data[i].title;
@@ -63,6 +81,38 @@ document.addEventListener("DOMContentLoaded", function () {
 
             videoDisplay.appendChild(document.createElement("hr"));
         }
+    })
+    .catch(error => console.error("Error fetching JSON data:", error));
+
+
+    fetch('resources/json/contactinfo.json')
+    .then(response => response.json())
+    .then(data => {
+            const contactDisplay = document.getElementById("contactDisplay");
+
+            const titleElement = document.createElement("h3");
+            titleElement.textContent = "Contact";
+
+            const divElement = document.createElement("div");
+            divElement.className = "inlinetext";
+            
+            const labelElement = document.createElement("p");
+            labelElement.textContent = "Email";
+
+            const linkElement = document.createElement("a");
+            linkElement.textContent = data.email;
+            linkElement.href = data.email;
+
+            divElement.appendChild(labelElement);
+            divElement.appendChild(linkElement);
+
+            //const textElement = document.createElement("p");
+            //textElement.textContent = "...or just contact me in person";
+
+            contactDisplay.appendChild(titleElement);
+            contactDisplay.appendChild(divElement);
+            contactDisplay.appendChild(document.createElement("hr"));
+            //contactDisplay.appendChild(textElement);
     })
     .catch(error => console.error("Error fetching JSON data:", error));
 });
