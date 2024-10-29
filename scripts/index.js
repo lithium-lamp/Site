f75f0815f5fdad62dcc7b0d668acda21b2df5fd6
Victor <victorlindholmutv@gmail.com>
2024-10-29 16:47:49 +0100

    Adding project descriptions 	modified:   index.html 	modified:   scripts/index.js 	modified:   styling/demo.css 	modified:   styling/main.css 	modified:   styling/projects.css 	modified:   styling/style.css

--- scripts/index.js
+++ scripts/index.js
@@ -179,3 +179,20 @@ function toggleScroll(checkbox) {
         startScroll();
     }
 }
+
+
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const square = entry.target.querySelector('.square');
+  
+      if (entry.isIntersecting) {
+        square.classList.add('square-animation');
+        return;
+      }
+
+      //square.classList.remove('square-animation');
+    });
+  });
+  
+  observer.observe(document.querySelector('.square-wrapper'));
